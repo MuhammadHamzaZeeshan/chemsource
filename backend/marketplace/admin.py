04127394f1051974619af7dcc394a9cd1ddf7beb
Muhammad Hamza Zeeshan <hamzazeeshan675@gmail.com>
2026-05-24 15:50:35 +0500

    register models in admin

--- backend/marketplace/admin.py
+++ backend/marketplace/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.CustomUser)
+admin.site.register(models.PriceListing)
+admin.site.register(models.ProcurementOrder)
+admin.site.register(models.ProductListing)
